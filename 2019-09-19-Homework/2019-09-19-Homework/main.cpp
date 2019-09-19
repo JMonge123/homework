7f1e78741d3913e0171bcf362473a4caa07bfc1d
JMonge123 <55443434+JMonge123@users.noreply.github.com>
2019-09-19 08:37:01 -0400

    Add files via upload

---
+++ 2019-09-19-Homework/2019-09-19-Homework/main.cpp
@@ -0,0 +1,22 @@
+// Joshua Monge
+// Intro to computer science
+// Period 1
+// Mr. Williams
+// 2019-09-19-Homework
+
+#include<iostream>
+int main()
+{
+	std::cout << "Name;Joshua Monge" << std::endl;
+	std::cout << "Class schedule" << std::endl;
+	std::cout << "Period 1; Intro to computer science" << std::endl;
+	std::cout << "Period 2; Earth science" << std::endl;
+	std::cout << "Period 3; House" << std::endl;
+	std::cout << "period 4; gym" << std::endl;
+	std::cout << "Period 5; Lunch" << std::endl;
+	std::cout << "Period 6; Health" << std::endl;
+	std::cout << "Period 7; Spanish" << std::endl;
+	std::cout << "Period 8; Algebra 2" << std::endl;
+	std::cout << "Period 9; ELA" << std::endl;
+	std::cout << "Period 10; Global History" << std::endl;
+}
